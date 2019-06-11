cdc9a66645fdd44f3edea1751eb141108d4fcc27
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-06-11 15:25:06 -0400

    added Question data model

---
+++ src/main/java/com/example/myapp/models/Question.java
@@ -0,0 +1,11 @@
+package com.example.myapp.models;
+
+public class Question {
+  private String question;
+  private int points;
+
+  public Question(String question, int points) {
+    this.question = question;
+    this.points = points;
+  }
+}
