c63fabb37ce9ecce3712cf4bc2cb93f288a10c59
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-06-13 13:38:13 -0400

    checkpointing

--- src/main/java/com/example/myapp/repositories/UserRepository.java
+++ src/main/java/com/example/myapp/repositories/UserRepository.java
@@ -1,8 +1,20 @@
 package com.example.myapp.repositories;
 
 import com.example.myapp.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.*;
+import org.springframework.data.repository.query.Param;
 
-public class UserRepository extends CrudRepository<User, Integer>{
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+  @Query(value = "select * from user where username=:username", nativeQuery = true)
+  public List<User> findUserByUsername(@Param("username") String username);
+
+  @Query(value = "select user from User user where user.username=:username and user.password=:password")
+  public List<User> findUserByCredentials(@Param("username") String username,
+                                    @Param("password") String password);
+
+
 
 }
---
+++ src/main/java/com/example/myapp/services/UserService.java
@@ -0,0 +1,36 @@
+package com.example.myapp.services;
+
+import com.example.myapp.models.User;
+import com.example.myapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+public class UserService {
+  @Autowired
+  UserRepository userRepo;
+
+  @GetMapping("api/users/")
+  public List<User> findAllUsers(@RequestParam(name="username", required = false) String username,
+                                 @RequestParam(name="password", required = false) String password) {
+    if (username != null) {
+      if (password != null) {
+        return userRepo.findUserByCredentials(username, password);
+      }
+
+      return userRepo.findUserByUsername(username);
+    }
+    return (List<User>) userRepo.findAll();
+  }
+
+  @PostMapping("api/users/")
+  public User createUser(@RequestBody User user) {
+    return userRepo.save(user);
+  }
+
+
+}
